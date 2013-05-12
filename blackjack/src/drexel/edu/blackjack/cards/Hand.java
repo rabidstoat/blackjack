5a031ca258d50c82399c63960bd214d48acdab5e
CLaz <klaz39054@gmail.com>
2013-05-12 18:27:57 +0300

    Update Hand.java

--- blackjack/src/drexel/edu/blackjack/cards/Hand.java
+++ blackjack/src/drexel/edu/blackjack/cards/Hand.java
@@ -6,5 +6,77 @@ package drexel.edu.blackjack.cards;
  *
  */
 public class Hand {
+  private ArrayList<Card> faceupCards;
+  private ArrayList<Card > facedownCards;
+	
+	public Hand(User user) {
+		
+	}
+	
+	public void addFaceupCard(Card card) {
+		
+	}
+	
+	public void removefaceupCard(Card card) {
+		
+	}
+	
+	public void clearFaceupCards() {
+		
+	}
+	
+	public ArrayList<Card> getFaceupCards() {
+		return null;
+		
+	}
+	
+	public ArrayList<Card> setFaceupCards() {
+		return null;
+	}
+	
+	public void addFacedownCard(Card card) {
+		
+	}
+	
+	public void removeFacedownCard(Card card) {
+		
+	}
+	
+	public void clearFacedownCards() {
+		
+	}
+	
+	public ArrayList<Card> getFacedownCards() {
+		return null;
+	}
+	
+	public ArrayList<Card> setFacedownCards() {
+		return null;
+	}
+	
+	public void clearAllCards() {
+		
+	}
+	
+	public int getTotalPoints() {
+		return (Integer) null;
+	}
+	
+	public boolean getIsBusted() {
+		return (Boolean) null;
+	}
+	
+	public boolean isBlackJack() {
+		return (Boolean) null;
+	}
+	
+	public User getUser() {
+		return null;
+	}
+	
+	public void setUser(User user) {
+		
+	}
+	
 
 }
