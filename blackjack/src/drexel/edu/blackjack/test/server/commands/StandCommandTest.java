73c005dab5ae241677f423d7c5f3245f6962bdd1
rabidstoat <rabidstoat@gmail.com>
2013-05-11 22:41:23 -0400

    Whee initial tests.

---
+++ blackjack/src/drexel/edu/blackjack/test/server/commands/StandCommandTest.java
@@ -0,0 +1,24 @@
+package drexel.edu.blackjack.test.server.commands;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StandCommandTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
