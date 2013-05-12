813ee7a456334c92ca33a09035be59895ffd44a2
rabidstoat <rabidstoat@gmail.com>
2013-05-12 01:56:08 -0400

    Placeholder.

---
+++ blackjack/src/drexel/edu/blackjack/db/game/GameState.java
@@ -0,0 +1,14 @@
+package drexel.edu.blackjack.db.game;
+
+/**
+ * Used to pass game state information from
+ * the server to client, to show what bets
+ * are made, what cards are dealt, what users
+ * are joining and leaving, etc.
+ * 
+ * @author Jennifer
+ *
+ */
+public class GameState {
+
+}
