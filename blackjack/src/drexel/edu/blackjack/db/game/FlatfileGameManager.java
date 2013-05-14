3652065a52313eb5b7d7f5ecd50a679eabd7812e
rabidstoat <rabidstoat@gmail.com>
2013-05-13 22:33:36 -0400

    Added comments and method signatures for the game manager classes too, while I was there.

--- blackjack/src/drexel/edu/blackjack/db/game/FlatfileGameManager.java
+++ blackjack/src/drexel/edu/blackjack/db/game/FlatfileGameManager.java
@@ -1,5 +1,54 @@
 package drexel.edu.blackjack.db.game;
 
+import java.util.List;
+
+/**
+ * Game metadata is stored out in a flat file. The format
+ * of the flat file is:
+ * 
+ * <Duc: Add documentation here about the format of the
+ * file.>
+ * 
+ * @author DAN
+ */
 public class FlatfileGameManager implements GameManagerInterface {
 
+	// Keep a single instance around for the singleton design pattern
+	private static FlatfileGameManager gameManager = null;
+
+	/**
+	 * Following the singleton design pattern, the constructor
+	 * is kept private.
+	 */
+	private FlatfileGameManager() {
+		// TODO: Implement. It's okay to use a hard-coded filename.
+	}
+	
+	@Override
+	public boolean load() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<GameMetadata> getGames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Following the singleton pattern, return a reference to
+	 * the singleton game manager.
+	 * 
+	 * @return A reference to the only game manager that should
+	 * be instantiated.
+	 */
+	public static GameManagerInterface getDefaultGameManager() {
+		
+		if( gameManager == null ) {
+			gameManager = new FlatfileGameManager();
+		}
+		
+		return gameManager;
+	}
 }
--- blackjack/src/drexel/edu/blackjack/db/game/GameManagerInterface.java
+++ blackjack/src/drexel/edu/blackjack/db/game/GameManagerInterface.java
@@ -1,11 +1,46 @@
 package drexel.edu.blackjack.db.game;
 
+import java.util.List;
+
 /**
- * What anything that implements a persistent
- * game manager should implement.
+ * Interface that any persistent store for games has to implement.
+ * 
+ * The data that has to be stored is a number of game descriptors,
+ * of which all the data that is in the GameMetadata class has
+ * to be stored. 
+ * 
  * @author Jennifer
- *
  */
 public interface GameManagerInterface {
+
+	/**
+	 * This loads the data from the persistent storage (e.g.,
+	 * the database or flatfile or whatever) into memory,
+	 * where it can be accessed programatically. The actual
+	 * initialization of the game manager will need to be done
+	 * on the class that implements it, and called prior to
+	 * loading. Recommendation is to do this in the constructor.
+	 * 
+	 *  @return True if the games were loaded successfully,
+	 *  False is there was any error in the loading that has
+	 *  the internal representation in a possibly inconsistent
+	 *  or incorrect state.
+	 */
+	public boolean load();
+	
+	/**
+	 * This returns a list of the GameMetadata objects
+	 * that were loaaded with the load() method. Though
+	 * you can call this before you've loaded the games,
+	 * it'll just return null.
+	 * 
+	 * @return If the load() command has been used, this
+	 * should return a valid list of games, though perhaps
+	 * of size zero if no games were loaded. If the load()
+	 * command was not called, or if it failed, it should
+	 * return null.
+	 */
+	public List<GameMetadata> getGames();
+	
 
 }
