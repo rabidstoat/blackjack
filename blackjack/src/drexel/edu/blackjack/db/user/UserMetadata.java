3fb8b603582fe5f7d2ff66b0e14567b3c29d97b5
rabidstoat <rabidstoat@gmail.com>
2013-05-13 22:41:52 -0400

    Implemented this also for Account command, since it took all of 30 seconds.

--- blackjack/src/drexel/edu/blackjack/db/user/UserMetadata.java
+++ blackjack/src/drexel/edu/blackjack/db/user/UserMetadata.java
@@ -6,5 +6,63 @@ package drexel.edu.blackjack.db.user;
  * @author DAN
  */
 public class UserMetadata {
+	
+	private String userame;
+	private String password;
+	private String fullname;
+	private int balance;		// Integer because we only deal in whole dollars
+	
+	// TODO: Maybe constructor? 
+
+	// Automated getters/setters below
+	
+	/**
+	 * @return the userame
+	 */
+	public String getUserame() {
+		return userame;
+	}
+	/**
+	 * @param userame the userame to set
+	 */
+	protected void setUserame(String userame) {
+		this.userame = userame;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	protected void setPassword(String password) {
+		this.password = password;
+	}
+	/**
+	 * @return the fullname
+	 */
+	public String getFullname() {
+		return fullname;
+	}
+	/**
+	 * @param fullname the fullname to set
+	 */
+	protected void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	/**
+	 * @return the balance
+	 */
+	public int getBalance() {
+		return balance;
+	}
+	/**
+	 * @param balance the balance to set
+	 */
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
 
 }
