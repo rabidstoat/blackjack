0d1730f2c31491a7542b880a6576bb9c07767a84
CLaz <klaz39054@gmail.com>
2013-05-14 23:34:53 +0300

    Hopefully reflects the logic specified for this class...

--- blackjack/src/drexel/edu/blackjack/server/commands/UsernameCommand.java
+++ blackjack/src/drexel/edu/blackjack/server/commands/UsernameCommand.java
@@ -9,11 +9,20 @@ import drexel.edu.blackjack.server.BlackjackProtocol.STATE;
 public class UsernameCommand extends BlackjackCommand {
 	
 	private static final String COMMAND_WORD = "USERNAME";
+	// Takes command words, like 'BET' or 'USERNAME'
+	private String commandWord = null;
 
+	/**Check to see if in a state (get it from protocol object) 
+		 * where this command is valid; if not, send error message*/
 	@Override
 	public String processCommand(BlackjackProtocol protocol, CommandMetadata cm) {
-		// We need to implement something here....
-		return super.processCommand(protocol, cm);
+	String stateWord = protocol.getState().toString(); 
+			
+			//Compare to only allowed state for USERNAME command "WAITING_FOR_USERNAME"; send error message if not equal.	
+			if(!stateWord.equals("WAITING_FOR_USERNAME")) {
+				System.out.println("404 USERNAME is not expected/allowed in the " +  stateWord + " state");
+			}
+			return super.processCommand(protocol, cm);	
 	}
 
 	@Override
@@ -39,12 +48,32 @@ public class UsernameCommand extends BlackjackCommand {
 			}
 	}
 
-
-
+	
+	/**
+	* This returns a list of parameters that the command requires.
+	* For USERNAME there is a 'username' parameter
+	* that the command needs. The CAPABILITIES command makes use of this
+	* for sending the capabilities list
+	* 
+	* @return
+	*/
+		 
 	@Override
 	public List<String> getRequiredParameterNames() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		ArrayList<String> requiredParameterNames = new ArrayList<String>();
+			
+			//Parse the USERNAME command and enter the parameter tokens in List.
+			// Use this to extract whitespace-delineated tokens
+			
+			StringTokenizer strtok = new StringTokenizer(COMMAND_WORD);
+			if( strtok.hasMoreTokens() ) {
+				commandWord = strtok.nextToken();	// command word is always first
+			}
+			// See if any parameters are left
+			while( strtok.hasMoreTokens() ) {
+				requiredParameterNames.add( strtok.nextToken() );
+			}
+			return requiredParameterNames;
+		}
 
 }
