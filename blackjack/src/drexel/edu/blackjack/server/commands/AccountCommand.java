8b0c4ec77d9e9c33aa2e5fdf941bdd465e1e6945
rabidstoat <rabidstoat@gmail.com>
2013-05-14 00:22:05 -0400

    First attempt at implementing the account command. Hard to test anything but the 'not authenticated' error message.

--- blackjack/src/drexel/edu/blackjack/server/commands/AccountCommand.java
+++ blackjack/src/drexel/edu/blackjack/server/commands/AccountCommand.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 import drexel.edu.blackjack.server.BlackjackProtocol;
 import drexel.edu.blackjack.server.BlackjackProtocol.STATE;
+import drexel.edu.blackjack.server.ResponseCode;
 
 public class AccountCommand extends BlackjackCommand {
 
@@ -15,8 +16,35 @@ public class AccountCommand extends BlackjackCommand {
 
 	@Override
 	public String processCommand(BlackjackProtocol protocol, CommandMetadata cm) {
-		// We need to implement something here....
-		return super.processCommand(protocol, cm);
+		
+		// Step 0: If either object is null, it's an internal error
+		if( protocol == null || cm == null ) {
+			return new ResponseCode( ResponseCode.CODE.INTERNAL_ERROR, 
+					"AccountCommand.processCommand() received null arguments" ).toString();
+		}
+		
+		// Steps 1-2: Return an error in not in a valid state
+		if( !getValidStates().contains( protocol.getState()) ) {
+			return new ResponseCode( ResponseCode.CODE.NEED_TO_BE_AUTHENTICATED ).toString();
+		}
+		
+		// Step 3-4: Check syntax; irrelevant as no parameters
+		
+		// Step 5: Do work that needs doing
+		
+		// Look up the user balance
+		if( protocol.getUser() == null || protocol.getUser().getUserMetadata() == null ) {
+			return new ResponseCode( ResponseCode.CODE.INTERNAL_ERROR,
+					"AccountCommand.processCommand() had a problem with the protocol object").toString();
+		}
+		int balance = protocol.getUser().getUserMetadata().getBalance();
+		
+		// Step 6: Save out state variables? There are none
+		// Step 7: Update any change in state? There is none
+		
+		// Step 8: Format the user response code
+		ResponseCode code = ResponseCode.createAccountBalanceResponseCode( balance );
+		return code.toString();
 	}
 
 	@Override
@@ -49,7 +77,7 @@ public class AccountCommand extends BlackjackCommand {
 
 	@Override
 	public List<String> getRequiredParameterNames() {
-		// TODO Auto-generated method stub
+		// There aren't any parameters for this command
 		return null;
 	}
 
