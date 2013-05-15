eebf3f221b01ed052998cacef30371a85a09659a
CLaz <klaz39054@gmail.com>
2013-05-15 11:06:13 +0300

    Coded using ResponseCode CODE options allowed for PASSWORD

--- blackjack/src/drexel/edu/blackjack/server/commands/PasswordCommand.java
+++ blackjack/src/drexel/edu/blackjack/server/commands/PasswordCommand.java
@@ -1,18 +1,33 @@
 package drexel.edu.blackjack.server.commands;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import drexel.edu.blackjack.server.BlackjackProtocol;
 import drexel.edu.blackjack.server.BlackjackProtocol.STATE;
-
+import drexel.edu.blackjack.server.ResponseCode;
 public class PasswordCommand extends BlackjackCommand {
 
 	private static final String COMMAND_WORD = "PASSWORD";
+	
+	Set<STATE> validPasswordStates = null;
 
 	public String processCommand(BlackjackProtocol protocol, CommandMetadata cm) {
-		// We need to implement something here....
-		return super.processCommand(protocol, cm);
+		
+		//Step 0: If either object is null, it's an internal error
+		if (protocol == null || cm == null) {
+			return new ResponseCode( ResponseCode.CODE.INTERNAL_ERROR,
+					"PasswordCommand.processCommand() received null arguments").toString();	
+		}
+		
+		// Step 1-2: Return an error if not in valid state for PASSWORD command
+		if(!getValidStates().contains( protocol.getState()) ) {
+			return new ResponseCode( ResponseCode.CODE.NOT_EXPECTING_PASSWORD,
+					"PasswordCommand.processCommand() received out-of-context command").toString();
+		}
+		
+			return super.processCommand(protocol, cm);
 	}
 
 	@Override
@@ -22,14 +37,31 @@ public class PasswordCommand extends BlackjackCommand {
 
 	@Override
 	public Set<STATE> getValidStates() {
-		// TODO Auto-generated method stub
-		return null;
+		validPasswordStates = new HashSet<STATE>();
+		
+		//Add the only allowed state which is WAITING_FOR_PASSWORD
+		validPasswordStates.add(STATE.WAITING_FOR_PASSWORD);
+		
+		return validPasswordStates;
 	}
 
 	@Override
 	public List<String> getRequiredParameterNames() {
-		// TODO Auto-generated method stub
-		return null;
+	
+		ArrayList<String> requiredParameterNames = new ArrayList<String>();
+		
+		//Parse the PASSWORD command and enter the parameter tokens in List.
+		// Use this to extract whitespace-delineated tokens
+		
+		StringTokenizer strtok = new StringTokenizer(COMMAND_WORD);
+		if( strtok.hasMoreTokens() ) {
+			String commandWord = strtok.nextToken();	// command word is always first
+		}
+		// See if any parameters are left
+		while( strtok.hasMoreTokens() ) {
+			requiredParameterNames.add( strtok.nextToken() );
+		}
+		return requiredParameterNames;
 	}
 
 }
