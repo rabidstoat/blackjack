fa346b1394b2e839cdd0510345ea4b21776ba965
CLaz <klaz39054@gmail.com>
2013-05-15 10:09:16 +0300

    Changes after reviewing ResponseCode class memo by Jennifer

--- blackjack/src/drexel/edu/blackjack/server/commands/UsernameCommand.java
+++ blackjack/src/drexel/edu/blackjack/server/commands/UsernameCommand.java
@@ -1,79 +1,75 @@
 package drexel.edu.blackjack.server.commands;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import drexel.edu.blackjack.server.BlackjackProtocol;
 import drexel.edu.blackjack.server.BlackjackProtocol.STATE;
+import drexel.edu.blackjack.server.ResponseCode;
 
 public class UsernameCommand extends BlackjackCommand {
 	
 	private static final String COMMAND_WORD = "USERNAME";
-	
 
-	/**Check to see if in a state (get it from protocol object) 
+	Set<STATE> validUsernameStates = null;
+	
+	
+	
+		/**Check to see if in a state (get it from protocol object) 
 		 * where this command is valid; if not, send error message*/
-	@Override
-	public String processCommand(BlackjackProtocol protocol, CommandMetadata cm) {
-	
-	//Step 0: If either object is null, it's an internal error
-	if(protocol == null || cm == null) {
-		return new ResponseCode( ResponseCode.CODE.INTERNAL_ERROR,
-			 "UsernameCommand.processCommand() received null arguments" ).toString();
-			}
-	
-	String stateWord = protocol.getState().toString(); 
+		public String processCommand(BlackjackProtocol protocol, CommandMetadata cm) {
 			
-		// Step 1-2: Return an error if not in valid state for USERNAME command
+			//Step 0: If either object is null, it's an internal error
+			if (protocol == null || cm == null) {
+				return new ResponseCode( ResponseCode.CODE.INTERNAL_ERROR,
+					"UsernameCommand.processCommand() received null arguments").toString();
+			}
+			
+			// Step 1-2: Return an error if not in valid state for USERNAME command
 			if(!getValidStates().contains( protocol.getState()) ) {
 				return new ResponseCode( ResponseCode.CODE.NOT_EXPECTING_USERNAME,
-					"UsernameCommand.processCommand() received wrong command").toString();
-			}
+					"UsernameCommand.processCommand() received out-of-context command").toString();
+						}
 			return super.processCommand(protocol, cm);	
-	}
+		}
 
-	@Override
-	public String getCommandWord() {
-		return COMMAND_WORD;
-	}
 
-	/**
-	 * This returns a set of states that the command can validly be used in.
-	 * The CAPABILITIES command will use this to get a list of capabilities
-	 * for the current protocol state to return
-	 **/
-	@Override
-	public Set<STATE> getValidStates() {
-			STATE WAITING_FOR_USERNAME = null;
+		/**
+		 * This returns a set of states that the command can validly be used in.
+		 * The CAPABILITIES command will use this to get a list of capabilities
+		 * for the current protocol state to return
+		 **/	
+
+		public Set<STATE> getValidStates() {
 				
-				Set<STATE> validUsernameStates = new HashSet<STATE>();
+				validUsernameStates = new HashSet<STATE>();
 				
 				//Add the only allowed state which is WAITING_FOR_USERNAME
 				validUsernameStates.add(STATE.WAITING_FOR_USERNAME);
 				
+				
 				return validUsernameStates;
 			}
-	}
-	
-	/**
-	* This returns a list of parameters that the command requires.
-	* For USERNAME there is a 'username' parameter
-	* that the command needs. The CAPABILITIES command makes use of this
-	* for sending the capabilities list
-	* 
-	* @return
-	*/
-		 
-	@Override
-	public List<String> getRequiredParameterNames() {
-		ArrayList<String> requiredParameterNames = new ArrayList<String>();
+		
+		/**
+		 * This returns a list of parameters that the command requires.
+		 * For USERNAME there is a 'username' parameter
+		 * that the command needs. The CAPABILITIES command makes use of this
+		 * for sending the capabilities list
+		 * 
+		 * @return
+		 */
+		public ArrayList<String> getRequiredParameterNames() {
+			
+			ArrayList<String> requiredParameterNames = new ArrayList<String>();
 			
 			//Parse the USERNAME command and enter the parameter tokens in List.
 			// Use this to extract whitespace-delineated tokens
 			
 			StringTokenizer strtok = new StringTokenizer(COMMAND_WORD);
 			if( strtok.hasMoreTokens() ) {
-				commandWord = strtok.nextToken();	// command word is always first
+				String commandWord = strtok.nextToken();	// command word is always first
 			}
 			// See if any parameters are left
 			while( strtok.hasMoreTokens() ) {
@@ -81,5 +77,6 @@ public class UsernameCommand extends BlackjackCommand {
 			}
 			return requiredParameterNames;
 		}
-
+		
 }
+
