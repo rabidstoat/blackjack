41abab1d00c881e2f3683dd49a987faf54250b23
CLaz <klaz39054@gmail.com>
2013-05-16 23:44:48 +0300

    Organized imports

--- blackjack/src/drexel/edu/blackjack/server/commands/UsernameCommand.java
+++ blackjack/src/drexel/edu/blackjack/server/commands/UsernameCommand.java
@@ -2,7 +2,6 @@ package drexel.edu.blackjack.server.commands;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.StringTokenizer;
 
