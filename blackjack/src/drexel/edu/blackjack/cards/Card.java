4b8b0a5b81b5dd18c0bddca5b934e9232bcf4e01
CLaz <klaz39054@gmail.com>
2013-05-17 01:52:05 +0300

    Update Card.java

--- blackjack/src/drexel/edu/blackjack/cards/Card.java
+++ blackjack/src/drexel/edu/blackjack/cards/Card.java
@@ -3,41 +3,89 @@ package drexel.edu.blackjack.cards;
 /**
  * A playing card.
  * @author Jennifer
+ * @author CLaz
  *
  */
 public class Card implements Comparable {
 	
-	public enum Rank {
+	public enum RANK {
 		  
-		ACE,	// = Ace
-		TWO,	// = 2
-		THREE,	// = 3
-		FOUR,	// = 4
-		FIVE,	// = 5
-		SIX,	// = 6
-		SEVEN,	// = 7
-		EIGHT,	// = 8
-		NINE,	// = 9
-		TEN,	// = 10
-		JACK,	// = Jack
-		QUEEN,	// = Queen
-		KING;	// = King
+		ACE (1),	
+		TWO(2),		
+		THREE(3),	
+		FOUR(4),	
+		FIVE(5),	
+		SIX(6),		
+		SEVEN(7),	
+		EIGHT(8),	
+		NINE(9),	
+		TEN(10),	
+		JACK(10),	
+		QUEEN(10),	
+		KING(10);	
+		
+		private final int rank;
+		
+		RANK( int rank ) {
+			this.rank = rank;	
+		}
+		
+		public void setRank( RANK rank ){
+			
+		}
+		
+		public int getRank() {
+			return rank;
+		}
 
 	}
 	
-	public enum Suit {
+	public enum SUIT {
 		  
-		 CLUBS,		//Clubs
-		 SPADES,	//Spades
-		 HEARTS,	//Hearts
-		 DIAMONDS;	//Diamonds
-		
+		 CLUBS('C'),		
+		 SPADES('S'),	
+		 HEARTS('H'),	
+		 DIAMONDS('D'), ;
+		 
+		 private final char suit;
+		 
+		 SUIT( char suit ) {
+			 this.suit = suit;
+		 }
+		 
+		 public char getSuit() {
+			 return suit;
+		 }
+		 
+		 public void setSuit ( SUIT suit ) {
+			 
+		 }
 
+	}
+	
+		
+	public Card(SUIT suit, RANK rank){
+		
+		
+		
+	}
+	
+	public int[] getValues(){
+		return null;
+		
+	}
+	
+	public String toString() {
+		return null;
+	}
+	
+	public int hashCode() {
+		return (Integer) null;
 	}
 
 	@Override
 	public int compareTo(Object arg0) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
