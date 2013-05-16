dc62ed9168ab936ea4a025cff132b996d3ae1a45
DAN (Duc Anh Nguyen) <geruk@geruk.me>
2013-05-16 04:08:30 -0400

    Added UserDB and basic test

--- blackjack/src/drexel/edu/blackjack/test/db/user/TestFlatfileUserManager.java
+++ blackjack/src/drexel/edu/blackjack/test/db/user/TestFlatfileUserManager.java
@@ -6,10 +6,30 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import drexel.edu.blackjack.db.user.FlatfileUserManager;
+import drexel.edu.blackjack.db.user.UserManagerInterface;
+import drexel.edu.blackjack.db.user.UserMetadata;
+
 public class TestFlatfileUserManager {
+	
+	UserMetadata u1, u2;
+	String[] u1a = new String[]{"name1","pass1","fullname1"};
+	String[] u2a = new String[]{"fdsfadsf", "fdsf3adf", "fdsf3fdsf"};
 
 	@Before
 	public void setUp() throws Exception {
+		u1 = new UserMetadata.Builder()
+			.setBalance(20)
+			.setUsername(u1a[0])
+			.setPassword(u1a[1])
+			.setFullname(u1a[2])
+			.build();
+		u2 = new UserMetadata.Builder()
+		.setBalance(20)
+		.setUsername(u2a[0])
+		.setPassword(u2a[1])
+		.setFullname(u2a[2])
+		.build();
 	}
 
 	@After
@@ -18,7 +38,15 @@ public class TestFlatfileUserManager {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		UserManagerInterface f = FlatfileUserManager.getDefaultUserManager();
+		f.add(u1);
+		assertTrue(f.save());
+		assertTrue(f.load());
+		assertTrue(f.loginUser(u1.getUsername(), u1.getPassword()).getFullname().equals(u1a[2]));
+		f.add(u2);
+		assertTrue(f.save());
+		assertTrue(f.load());
+		assertTrue(f.loginUser(u2.getUsername(), u2.getPassword()).getFullname().equals(u2a[2]));
 	}
 
 }
