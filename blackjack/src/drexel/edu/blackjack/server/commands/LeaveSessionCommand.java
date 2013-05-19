ff661b230fc1dc26b96aea4018502aa140e8817b
rabidstoat <rabidstoat@gmail.com>
2013-05-19 11:04:36 -0400

    Untested implementation.

--- blackjack/src/drexel/edu/blackjack/server/commands/LeaveSessionCommand.java
+++ blackjack/src/drexel/edu/blackjack/server/commands/LeaveSessionCommand.java
@@ -1,18 +1,88 @@
+
 package drexel.edu.blackjack.server.commands;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import drexel.edu.blackjack.server.BlackjackProtocol;
+import drexel.edu.blackjack.server.ResponseCode;
 import drexel.edu.blackjack.server.BlackjackProtocol.STATE;
+import drexel.edu.blackjack.server.game.Game;
+import drexel.edu.blackjack.server.game.User;
 
 public class LeaveSessionCommand extends BlackjackCommand {
 
 	private static final String COMMAND_WORD = "LEAVESESSION";
 
 	public String processCommand(BlackjackProtocol protocol, CommandMetadata cm) {
-		// We need to implement something here....
-		return super.processCommand(protocol, cm);
+		// Step 0: If either object is null, it's an internal error
+		if (protocol == null || cm == null) {
+			return new ResponseCode(ResponseCode.CODE.INTERNAL_ERROR,
+					"JoinSessionCommand.processCommand() received null arguments")
+					.toString();
+		}
+
+		// Steps 1-2: Return an error in not in a valid state
+		if (!getValidStates().contains(protocol.getState())) {
+			return getResponseStingForInvalidState(protocol.getState());
+		}
+
+		// Step 3-4: Check syntax; must have 0 parameters
+		if( cm.getParameters() != null && cm.getParameters().size() > 0 ) {
+			return new ResponseCode( ResponseCode.CODE.SYNTAX_ERROR, 
+					"The LEAVESESSION command requires no parameters." ).toString();
+		}
+
+		// Step 5: Do work that needs doing
+		
+		// They need to be in a game if we're here. If they don't have a valid game
+		// object we have some sort of internal error
+		User user = protocol.getUser();
+		if( user == null ) {
+			return new ResponseCode( ResponseCode.CODE.INTERNAL_ERROR, 
+					"In the LEAVESESSION command, the user was null." ).toString();
+		}
+		Game game = user.getGame();
+		if( game == null ) {
+			return new ResponseCode( ResponseCode.CODE.INTERNAL_ERROR, 
+					"In the LEAVESESSION command, the game was null." ).toString();
+		}
+		
+		// There are a few separate response codes that can get returned, based
+		// on whether the user is forfeiting a bet. This will be sorted out elsewhere
+		ResponseCode code = game.removePlayer( user );
+		
+		// Step 6: Save out state variables? There are none
+
+		// Step 7: Update the change in state
+		protocol.setState( STATE.NOT_IN_SESSION );
+
+		// Step 8: Format the user response code
+		// Use what was returned earlier, unless it's null (which is a syntax error)
+		if( code == null ) {
+			code = new ResponseCode( ResponseCode.CODE.INTERNAL_ERROR,
+					"game.removePlayer() returned a null in LeaveSessionCommand.processCommand()" );
+		}
+		return code.toString();
+	}
+
+	/**
+	 * The system is in some state where leave a session is not allowed. Need
+	 * to return an error message that's appropriate.
+	 * 
+	 * @param protocol
+	 * @return
+	 */
+	private String getResponseStingForInvalidState(BlackjackProtocol.STATE state) {
+		
+		// One case is that they're not authenticated...
+		if( state.equals(STATE.WAITING_FOR_PASSWORD) || state.equals(STATE.WAITING_FOR_USERNAME) ) {
+			return new ResponseCode( ResponseCode.CODE.NEED_TO_BE_AUTHENTICATED ).toString();
+		}
+		
+		// The other case is that they're not in a session
+		return new ResponseCode( ResponseCode.CODE.USER_NOT_IN_GAME_ERROR).toString();
 	}
 
 	@Override
@@ -22,13 +92,20 @@ public class LeaveSessionCommand extends BlackjackCommand {
 
 	@Override
 	public Set<STATE> getValidStates() {
-		// TODO Auto-generated method stub
-		return null;
+		Set<STATE> validStates = new HashSet<STATE>();
+		// Available in any of the IN_SESSION states
+		validStates.add( STATE.IN_SESSION_AFTER_YOUR_TURN );
+		validStates.add( STATE.IN_SESSION_AND_YOUR_TURN );
+		validStates.add( STATE.IN_SESSION_AS_OBSERVER );
+		validStates.add( STATE.IN_SESSION_AWAITING_BETS );
+		validStates.add( STATE.IN_SESSION_BEFORE_YOUR_TURN );
+		validStates.add( STATE.IN_SESSION_DEALER_BLACKJACK );
+		validStates.add( STATE.IN_SESSION_SERVER_PROCESSING);
+		return validStates;
 	}
 
 	@Override
 	public List<String> getRequiredParameterNames() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
