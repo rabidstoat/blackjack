48099dd6066740dd3f0d8fcf865c312cdf06c03f
rabidstoat <rabidstoat@gmail.com>
2013-05-19 11:59:21 -0400

    Began work on this screen...

--- blackjack/src/drexel/edu/blackjack/client/screens/NotInSessionScreen.java
+++ blackjack/src/drexel/edu/blackjack/client/screens/NotInSessionScreen.java
@@ -13,19 +13,46 @@ import drexel.edu.blackjack.server.ResponseCode;
  */
 public class NotInSessionScreen extends AbstractScreen {
 	
+	/**********************************************************************
+	 * Private variables.
+	 *********************************************************************/
+
+	// This screen can be in one of these two states
+	private static final int MAIN_MENU	= 0;
+	private static final int JOIN_GAME	= 1;
+
+	// The different options for this menu
+	private static String JOIN_OPTION			= "J";
+	private static String VERSION_OPTION		= "V";
+	private static String CAPABILITIES_OPTION	= "C";
+	private static String QUIT_OPTION			= "Q";
+	private static String MENU_OPTION			= "?";
+	
+	// Which of the states the screen is in
+	private int state;
+	
 	// And keep a copy to itself for the singleton pattern
 	private static NotInSessionScreen notInSessionScreen = null;
 	
-	public NotInSessionScreen( BlackjackCLClient client, ClientInputFromServerThread thread,
+	/**********************************************************************
+	 * Private constructor.
+	 *********************************************************************/
+	
+	private NotInSessionScreen( BlackjackCLClient client, ClientInputFromServerThread thread,
 			ClientOutputToServerHelper helper ) {
 		
 		// It starts in the ENTER_USERNAME state, but inactive
 		super(client, thread,helper);
 		setScreenType( AbstractScreen.SCREEN_TYPE.NOT_IN_SESSION_SCREEN );
+		state = MAIN_MENU;
 		setIsActive( false );
 		
 	}
 
+	/**********************************************************************
+	 * Public methods.
+	 *********************************************************************/
+	
 	@Override
 	public void processMessage(ResponseCode code) {
 
@@ -57,8 +84,17 @@ public class NotInSessionScreen extends AbstractScreen {
 	 */
 	public void displayMenu() {
 
-		System.out.println( "Someone needs to write the NotInSessionScreen.displayMenu() method." );
-		System.out.println( "For now, just type in raw protocol commands." );
+		if( state == JOIN_GAME ) {
+			System.out.println( "Have not implemented join game menu yet, whoops!" );
+			state = MAIN_MENU;
+		} else {
+			System.out.println( "Please enter the letter or symbol of the option to perform:" );
+			System.out.println( VERSION_OPTION + ") See what version of the game is running (for debug purposes)" );
+			System.out.println( CAPABILITIES_OPTION + ") See what capabilities the game implements (for debug purposes)" );
+			System.out.println( JOIN_OPTION + ") Join a game" );
+			System.out.println( QUIT_OPTION + ") Quit playing" );
+			System.out.println( MENU_OPTION + ") Repeat this menu of options" );
+		}
 		
 	}
 
@@ -88,16 +124,59 @@ public class NotInSessionScreen extends AbstractScreen {
 		
 		// For us, resetting the screen involves showing the menu again
 		System.out.println( "Whoops, the user interface got confused. Let's try this again." );
+		state = MAIN_MENU;
 		displayMenu();
 		
 	}
 
 	@Override
 	public void handleUserInput(String str) {
-		
-		// TODO: Something more sensible
-		helper.sendRawText( str );
 
+		if( state == MAIN_MENU ) {
+
+			if( str == null ) {
+				reset();
+			}
+			
+			if( str.trim().equals(CAPABILITIES_OPTION) ) {
+				sendCapabilitiesRequest();
+			} else if( str.trim().equals(JOIN_OPTION) ) {
+				sendListGamesRequest();
+			} else if( str.trim().equals(MENU_OPTION) ) {
+				displayMenu();
+			} else if( str.toCharArray().equals(QUIT_OPTION) ) {
+				quit();
+			} else if( str.toCharArray().equals(VERSION_OPTION) ) {
+				sendVersionRequest();
+			} else {
+				System.out.println( "Unrecognized user input: " + str );
+				displayMenu();
+			}
+			
+		} else {
+			System.out.println( "Regretably have not implemented any options here." );
+		}
+
+	}
+
+	private void quit() {
+		System.out.println( "There is no quitting, mwahahaha!" );
+		displayMenu();
+	}
+
+	private void sendVersionRequest() {
+		System.out.println( "One moment, fetching the version from the server..." );
+		helper.sendVersionRequest();
+	}
+
+	private void sendListGamesRequest() {
+		System.out.println( "One moment, fetching a list of games from the server..." );
+		helper.sendListGamesRequest();
+	}
+
+	private void sendCapabilitiesRequest() {
+		System.out.println( "One moment, fetching a list of capabilities from the server..." );
+		helper.sendCapabilitiesRequest();
 	}
 
 }
