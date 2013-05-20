7e86e8af944849364cd1c263c76dbd2f0ee69874
DAN (Duc Anh Nguyen) <geruk@geruk.me>
2013-05-20 02:47:41 -0400

    Added DealerShoe

--- blackjack/src/drexel/edu/blackjack/cards/SimpleDealerShoe.java
+++ blackjack/src/drexel/edu/blackjack/cards/SimpleDealerShoe.java
@@ -1,47 +1,63 @@
 package drexel.edu.blackjack.cards;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class SimpleDealerShoe implements DealerShoeInterface {
+	
+	private final int numDecks;
+	private final ArrayList<Card> deck;
+	private final ArrayList<Card> bin;
+	
+	public SimpleDealerShoe(int numDeck) {
+		numDecks = numDeck;
+		deck = new ArrayList<Card>();
+		bin = new ArrayList<Card>();
+		for (int i=0; i < numDeck; i++) {
+			for (Card.SUIT suit:Card.SUIT.values()) {
+				for (Card.RANK rank: Card.RANK.values()) {
+					deck.add(new Card(rank, suit));
+				}
+			}
+		}
+	}
 
 	@Override
 	public void shuffle() {
-		// TODO Auto-generated method stub
-		
+		Collections.shuffle(deck);
 	}
 
 	@Override
 	public Card dealTopCard() {
-		// TODO Auto-generated method stub
-		return null;
+		if (isEmpty()) return null;
+		Card c = deck.remove(0);
+		bin.add(c);
+		return c;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return deck.size() == 0;
 	}
 
 	@Override
 	public int getNumberDecks() {
-		// TODO Auto-generated method stub
-		return 0;
+		return numDecks;
 	}
 
 	@Override
 	public int getNumberOfDealtCards() {
-		// TODO Auto-generated method stub
-		return 0;
+		return bin.size();
 	}
 
 	@Override
 	public float getPercentageOfDealtCards() {
-		// TODO Auto-generated method stub
-		return 0;
+		return bin.size()/(bin.size() + deck.size());
 	}
 
 	@Override
 	public int getTotalNumberOfCards() {
-		// TODO Auto-generated method stub
-		return 0;
+		return deck.size();
 	}
 
 }
