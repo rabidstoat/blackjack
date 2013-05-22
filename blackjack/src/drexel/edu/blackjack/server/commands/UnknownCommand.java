acbd848aad6834b1f24f6361be14b79539fbb2c9
77dragonflies <77dragonflies@gmail.com>
2013-05-22 01:26:33 -0400

    Update UnknownCommand.java

--- blackjack/src/drexel/edu/blackjack/server/commands/UnknownCommand.java
+++ blackjack/src/drexel/edu/blackjack/server/commands/UnknownCommand.java
@@ -1,43 +1,82 @@
 package drexel.edu.blackjack.server.commands;
 
-import java.util.List;
 import java.util.Set;
-
+import java.util.HashSet;
 import drexel.edu.blackjack.server.BlackjackProtocol;
 import drexel.edu.blackjack.server.BlackjackProtocol.STATE;
+import drexel.edu.blackjack.server.ResponseCode;
+import drexel.edu.blackjack.server.commands.BlackjackCommand;
+import drexel.edu.blackjack.server.commands.CommandMetadata;
 
-/**
- * This class handles the case where the user sent a command
- * that is not recognized. That is, the first token on the 
- * command string does not match any of the twelve commands
- * we have implemented.
- * 
- * @author Jennifer
- */
+
 public class UnknownCommand extends BlackjackCommand {
 
-	public String processCommand(BlackjackProtocol protocol, CommandMetadata cm) {
-		// We need to implement something here....
-		return super.processCommand(protocol, cm);
+	private static final String COMMAND_WORD = "UNKNOWN";
+	
+	private Set<STATE> validStates = null;
+	
+	@Override
+	public String processCommand( BlackjackProtocol protocol, CommandMetadata cm ) {
+		
+		//Step 0: if either object is null, there is an internal error
+		if( protocol == null || cm == null) {
+			return new ResponseCode( ResponseCode.CODE.INTERNAL_ERROR, 
+					"UnknownCommand.processCommand() received null arguments").toString();
+		}
+		
+		//Steps 1-2: Return error if not in a valid state 
+		//unknown command available in any connected state 
+		if( !getValidStates().contains( protocol.getState() )) {
+			return new ResponseCode( ResponseCode.CODE.NEED_TO_BE_AUTHENTICATED).toString();
+		}
+	
+		//Step 3 -4: check syntax, no parameters in unknown
+		
+		//step 5 - if system does not recognize command word - sends error response:
+		//command word in not recognized do response code 500)
+		//command word in not supported do response code 501)
+		//command word has syntax error do response code 502)
+		
+		//Step 6: Save out state variables? nothing to save in unknown
+		
+		//Step 7: Update any change in state - not state changes in unknown
+				
+		{	
+		//step 8 return response code- will only be using generic response
+			return 	new ResponseCode( ResponseCode.CODE.UNKNOWN_COMMAND ).toString();
+		}
 	}
-
+		
 	@Override
 	public String getCommandWord() {
-		// Since this is the command to use if no valid command is specified,
-		// it returns the special 'null' command word
-		return null;
+		return COMMAND_WORD;
 	}
-
+	
 	@Override
 	public Set<STATE> getValidStates() {
-		// TODO Auto-generated method stub
-		return null;
+		//Keep this around so we only create once
+		if( validStates == null ) {
+			validStates = new HashSet<STATE>();
+			
+			//version command works in any connected states
+			validStates.add( STATE.WAITING_FOR_USERNAME );
+			validStates.add( STATE.WAITING_FOR_PASSWORD );
+			validStates.add( STATE.NOT_IN_SESSION );
+			validStates.add( STATE.IN_SESSION_AS_OBSERVER );
+			validStates.add( STATE.IN_SESSION_AWAITING_BETS );
+			validStates.add( STATE.IN_SESSION_BEFORE_YOUR_TURN );
+			validStates.add( STATE.IN_SESSION_AND_YOUR_TURN );
+			validStates.add( STATE.IN_SESSION_DEALER_BLACKJACK );
+			validStates.add( STATE.IN_SESSION_SERVER_PROCESSING );
+			
+		}
+		return validStates;
 	}
-
+	
 	@Override
-	public List<String> getRequiredParameterNames() {
-		// TODO Auto-generated method stub
+	public java.util.List<String> getRequiredParameterNames() {
+		//Unknown has no required parameters
 		return null;
 	}
-
 }
+
