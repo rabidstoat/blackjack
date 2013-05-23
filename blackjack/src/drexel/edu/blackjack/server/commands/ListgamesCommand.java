b67f9fdaaaade459aa9f7e6c4b1dd3cdcf8b7a72
rabidstoat <rabidstoat@gmail.com>
2013-05-23 16:55:19 -0400

    Now returns a list of games.

--- blackjack/src/drexel/edu/blackjack/server/commands/ListgamesCommand.java
+++ blackjack/src/drexel/edu/blackjack/server/commands/ListgamesCommand.java
@@ -6,6 +6,9 @@ import java.util.Set;
 
 import drexel.edu.blackjack.server.BlackjackProtocol;
 import drexel.edu.blackjack.server.BlackjackProtocol.STATE;
+import drexel.edu.blackjack.server.ResponseCode;
+import drexel.edu.blackjack.server.game.ActiveGameCoordinator;
+import drexel.edu.blackjack.server.game.Game;
 
 public class ListgamesCommand extends BlackjackCommand {
 
@@ -14,8 +17,38 @@ public class ListgamesCommand extends BlackjackCommand {
 	private Set<STATE> validStates = null;
 
 	public String processCommand(BlackjackProtocol protocol, CommandMetadata cm) {
-		// We need to implement something here....
-		return super.processCommand(protocol, cm);
+		//Step 0: If either object is null, it's an internal error
+		if (protocol == null || cm == null) {
+			return new ResponseCode( ResponseCode.CODE.INTERNAL_ERROR,
+				"ListgamesCommand.processCommand() received null arguments").toString();
+		}
+		
+		// Steps 1-2: Return an error in not in a valid state
+		if( !getValidStates().contains( protocol.getState()) ) {
+			return new ResponseCode( ResponseCode.CODE.NEED_TO_BE_AUTHENTICATED ).toString();
+		}
+		
+		// Step 3-4: Check syntax; irrelevant as no parameters
+		
+		// Step 5: Do work that needs doing
+		// Just quick and dirty for testing, get all games
+		ActiveGameCoordinator coordinator = ActiveGameCoordinator.getDefaultActiveGameCoordinator();
+		Set<Game> games = coordinator.getAllGames();
+		
+		// Set up something to return
+		StringBuilder str = new StringBuilder( (games == null ? 0 : games.size()) + " games follow:\n");
+		if( games != null ) {
+			for( Game game : games ) {
+				str.append( game.getGameDescriptor() );
+			}
+		}
+		
+		// Step 6: Save out state variables? There are none
+		// Step 7: Update any change in state? There is none
+		
+		// Step 8: Format the user response code
+		ResponseCode code = new ResponseCode( ResponseCode.CODE.GAMES_FOLLOW, str.toString() );
+		return code.toString();
 	}
 
 	@Override
