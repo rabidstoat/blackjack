ce71888cce06ec8459f95af4c9803cadb4580bbd
rabidstoat <rabidstoat@gmail.com>
2013-06-03 12:19:18 -0400

    Did the package comments wrong, this looks nicer format-wise.

--- blackjack/src/drexel/edu/blackjack/client/locator/package-info.java
+++ blackjack/src/drexel/edu/blackjack/client/locator/package-info.java
@@ -1,16 +1,19 @@
 /*******************************************************************************
+ * Functionality for the client using a server locator service
+ * <P>
  * CS544 Computer Networks Spring 2013
- * 5/26/2013 - BlackjackProtocol.java
- * Group Members
- * o Jennifer Lautenschlager
- * o Constantine Lazarakis
- * o Carol Greco
- * o Duc Anh Nguyen
- * 
+ * <P>
+ * 6/3/2013
+ * <P>
  * <b>EXTRACREDIT</b>: This package provides the client-side support for the
  * extra credit problem of how to find a server to connect to without being
  * given a host. See the class files for more detail on how this is 
  * accomplished (though it's basically by multicasting a request to a specific
  * group and port, and then monitoring for any useful responses).
+ * @author Jennifer Lautenschlager
+ * @author Constantine Lazarakis
+ * @author Carol Greco
+ * @author Duc Anh Nguyen
+ * @version 1.0
  ******************************************************************************/
 package drexel.edu.blackjack.client.locator;
--- blackjack/src/drexel/edu/blackjack/server/locator/package-info.java
+++ blackjack/src/drexel/edu/blackjack/server/locator/package-info.java
@@ -1,17 +1,20 @@
 /*******************************************************************************
+ * Functionality for a server locator service
+ * <P>
  * CS544 Computer Networks Spring 2013
- * 5/26/2013 - BlackjackProtocol.java
- * Group Members
- * o Jennifer Lautenschlager
- * o Constantine Lazarakis
- * o Carol Greco
- * o Duc Anh Nguyen
- * 
+ * <P>
+ * 6/3/2013
+ * <P>
  * <b>EXTRACREDIT</b>: This package provides the server-side support for the
  * extra credit problem of how to find a server to connect to without being
  * given a host. See the class files for more detail on how this is 
  * accomplished (though it's basically by monitoring a multicast UDP port
  * and responding to messages that are looking for a server with an appropriate
  * address).
+ * @author Jennifer Lautenschlager
+ * @author Constantine Lazarakis
+ * @author Carol Greco
+ * @author Duc Anh Nguyen
+ * @version 1.0
  ******************************************************************************/
 package drexel.edu.blackjack.server.locator;
